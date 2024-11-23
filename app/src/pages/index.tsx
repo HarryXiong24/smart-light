63f31f8b218180f49bb7b5eb4c41b2b96172c594
Harry Xiong <harryxiong24@gmail.com>
2024-11-23 00:29:43 -0800

    feat: add api request to fix init value

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch } from 'antd-mobile';
 import styles from './index.module.scss';
 
 const Control = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const getLightState = async () => {
+    const res = await fetch('/api/control-light', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (res.ok) {
+      const result = await res.json();
+      setIsOpen(result.isOpen);
+    }
+  };
+
   const handleState = async (checked: boolean) => {
-    console.log('checked', checked);
     await fetch(`/api/handle-state?isOpen=${checked ? 1 : 0}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    setIsOpen(checked);
   };
+
+  useEffect(() => {
+    getLightState();
+  }, []);
 
   return (
     <div className={styles.control}>
       <div className={styles['operation-item']}>
-        <p className={styles.title}>Turn On/Off the light</p>
+        <p className={styles.title}>Turn on/off the light</p>
         <Switch
-          defaultChecked={false}
+          checked={isOpen}
           onChange={handleState}
           style={{
             '--checked-color': '#a03cf7',
